982327d0d5a188797f4d23c8d73d6b8b2e88c988
Igor Floriani <florianiigor@gmail.com>
2024-03-22 07:16:04 -0300

    deixando o jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroMaximo = 1000;
+let numeroMaximo = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 let mensagemNumeroMaximo = `Escolha um número entre 1 e ${numeroMaximo}`;
